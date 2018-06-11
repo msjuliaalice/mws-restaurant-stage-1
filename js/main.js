2ab300b62e60fe0a7e2bb0039e7839ad610b5162
msjuliaalice <msjuliaalice@gmail.com>
2018-06-10 19:09:41 -0500

    Responsive Images and Layout
    Images take up reasonable portions of the screen at breakpoints. Fixes #5
    Layout changes based on viewport size. Fixes #8

--- js/main.js
+++ js/main.js
@@ -155,7 +155,11 @@ createRestaurantHTML = (restaurant) => {
   li.append(image);
 
   const name = document.createElement('h1');
-  name.innerHTML = restaurant.name;
+  //name.innerHTML = restaurant.name;
+  const more = document.createElement('a');
+  more.innerHTML = restaurant.name;
+  more.href = DBHelper.urlForRestaurant(restaurant);
+  name.appendChild(more);
   li.append(name);
 
   const neighborhood = document.createElement('p');
@@ -166,10 +170,8 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+
+  //li.append(more)
 
   return li
 }
